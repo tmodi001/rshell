002de5007521f2bb8b95effe5720be23a54d1074
tmodi001 <tmodi001@ucr.edu>
2015-11-06 23:14:31 -0800

    Update single_command.sh

--- test/single_command.sh
+++ test/single_command.sh
@@ -1,1 +1,21 @@
-single_command.sh
+Script started on Sat 07 Nov 2015 07:10:48 AM UTC
+tmodi001@cs_private:~/workspace/cs100/assn1 $ g++e-std=c++11 -Wall -Werror -pedantic -g almost.cpp
+tmodi001@cs_private:~/workspace/cs100/assn1 $ ./a.out
+$ ls
+Makefile  README.md  a.out  almost.cpp	edit.cpp  rshell.cpp  single_command.sh  test.cpp  typescript
+$ eooor
+Invalid Prompt Entered!
+$ echo 1
+1
+$ mkdir test
+$ rmdir test
+$ echo hellowworld
+hello world
+$ echo this issworking
+this is working
+$ exit
+$ exit
+tmodi001@cs_private:~/workspace/cs100/assn1 $ exit
+exit
+
+Script done on Sat 07 Nov 2015 07:12:04 AM UTC
